fe101e2ac840dc7f279ee05a2556ed20c0989b08
Albert Hsu <iverson52000@gmail.com>
2021-07-25 10:47:21 -0700

    create testQuery.js

---
+++ testQuery.js
@@ -0,0 +1,16 @@
+const Knex = require('knex')
+const { Model } = require('objection')
+const knexConfig = require('./knexfile')
+const knex = Knex(knexConfig.development)
+Model.knex(knex)
+
+const Movie = require('./models/Movie')
+
+const fetchMovies = async () => {
+  let movies = await Movie.query().where('name', 'like', '%Martian%').withGraphFetched('actors')
+
+  console.log(movies)
+  process.exit(0)
+}
+
+fetchMovies()
